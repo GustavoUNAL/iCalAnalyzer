bafd4c27c18ec9857caf79ab516dd71ea396e2c7
GustavoUNAL <garteagae@unal.edu.co>
2025-02-05 11:54:41 -0500

    add cosrs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
 from app.core.config import get_settings
 
@@ -8,6 +9,17 @@ app = FastAPI(
     title="Calendar Analytics API",
     version=settings.API_VERSION,
     debug=settings.DEBUG_MODE
+)
+
+# Configurar las políticas de CORS
+origins = ["*"]  # Ajusta esta lista según los orígenes permitidos
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Incluir el router principal con un prefijo
